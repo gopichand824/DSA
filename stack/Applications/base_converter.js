71165e2b2c45fce6a24c156b1779644ad04c38b5
gopichand yadav <gopichand63063@gmail.com>
2024-10-23 13:49:15 +0530

    implement base convertre algorighm using stack data structure

--- stack/Applications/base_converter.js
+++ stack/Applications/base_converter.js
@@ -1,2 +1,25 @@
-const stack = require('../stack.js');
+const Stack = require('../stack.js');
 
+function baseConverter(decimal , base) {
+    if (decimal < 0 || base < 2 || base > 16) {
+        return 'Invalid input';
+    }
+     const digits = '0123456789ABCDEF';
+     let baseStr = '';
+     let result = new Stack();
+
+     while (decimal > 0 ) {
+        result.push(Math.floor(decimal % base));
+        decimal = Math.floor(decimal / base);
+     }
+
+     while (!result.isEmpty()){
+        baseStr += digits[result.pop()];
+     }
+
+     return baseStr;
+}
+
+// Test the function
+
+console.log(baseConverter(200, 16)); 
