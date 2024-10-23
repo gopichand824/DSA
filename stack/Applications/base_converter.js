05ec79007b1bcea46a316dd789b1e7e39dace22c
gopichand yadav <gopichand63063@gmail.com>
2024-10-23 19:49:50 +0530

    fix in base_converter() function for decimal = 0

--- stack/Applications/base_converter.js
+++ stack/Applications/base_converter.js
@@ -4,6 +4,9 @@ function baseConverter(decimal , base) {
     if (decimal < 0 || base < 2 || base > 16) {
         return 'Invalid input';
     }
+    if (decimal ===0) {
+      return 0;
+    }
      const digits = '0123456789ABCDEF';
      let baseStr = '';
      let result = new Stack();
@@ -23,3 +26,7 @@ function baseConverter(decimal , base) {
 // Test the function
 
 console.log(baseConverter(200, 16)); 
+for(let i = 0 ; i < 10; i++) {
+   console.log(`decimal ${i} to \n\tbinary     :${baseConverter(i, 2)} \n\toctal      :${baseConverter(i, 8)} \n\thexadecimal:${baseConverter(i, 16)}\n`);
+
+}
